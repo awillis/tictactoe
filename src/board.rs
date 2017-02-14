4a9de3c841c2bc6ec665c3b989a51186628ab3ae
Alan Willis <alan@amekoshi.com>
2017-02-13 23:59:40 -0800

    Square and Board types defined

--- src/board.rs
+++ src/board.rs
@@ -1,65 +1,69 @@
 #![allow(dead_code)]
 enum Legend {
-  North,
-  NEast,
-  East,
-  SEast,
-  South,
-  SWest,
-  West,
-  NWest
+    North,
+    NEast,
+    East,
+    SEast,
+    South,
+    SWest,
+    West,
+    NWest,
 }
 
 enum SquareState {
-  EMPTY, X, O, BORDER
+    EMPTY,
+    X,
+    O,
+    BORDER,
 }
 
-struct Square<'a> {
-  xpos: usize,
-  ypos: usize,
-  state: SquareState,
-  compass: &'a mut [Square<'a>; 8]
+type Compass = Option<[Square; 8]>;
+
+struct Square {
+    xpos: usize,
+    ypos: usize,
+    state: SquareState,
+    compass: Box<Compass>,
 }
 
-impl<'a> Square<'a> {
-
+impl Square {
     fn new() -> Self {
-    
-        Square{
+
+        Square {
             xpos: 0,
             ypos: 0,
-            state: EMPTY,
-            compass: &[]
+            state: SquareState::EMPTY,
+            compass: Box::new(None),
         }
     }
 }
 
-
-pub struct Board<'a> {
-  rows: usize,
-  cols: usize,
-  win_condition: u8,
-  grid: Vec<Square<'a>>
+pub struct Board {
+    rows: usize,
+    cols: usize,
+    win_condition: u8,
+    grid: Vec<Square>,
 }
 
-impl<'a> Board<'a> {
+impl Board {
+    pub fn new(rows: usize, cols: usize) -> Self {
 
-    pub fn new( rows: usize, cols: usize ) -> Self {
-    
-        let gridv = Vec::<Square<'a>>::new();
-        
-        for x in 0 .. rows {
-            for y in 0 .. cols {
-            
+        let mut gridv = Vec::<Square>::new();
+
+        for x in 0..rows {
+            for y in 0..cols {
+
                 let index = (x * rows) + y;
+
                 gridv[index] = Square::new()
             }
         }
-        
-        Board{
-            rows: rows, 
-            cols: cols, 
-            win_condition: 3, 
-            grid: gridv}
+
+        Board {
+            rows: rows,
+            cols: cols,
+            win_condition: 3,
+            grid: gridv,
+        }
     }
 }
